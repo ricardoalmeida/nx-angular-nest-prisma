3df038436f160592c001cebebdd2fc3dadef373b
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2021-02-17 09:36:49 +0100

    initial commit

---
+++ apps/nx-angular-nest-prisma/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-nest-prisma-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'nx-angular-nest-prisma';
+}
